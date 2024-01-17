4694c349b4b0e78c0d1918dbcf041b5cc09bfe5d
曦暮流年 <2515550574@qq.com>
2024-01-17 23:32:20 +0800

    收录网站+添加索引功能

--- js/tagNav.js
+++ js/tagNav.js
@@ -2,10 +2,16 @@
 axios.get("./json/allPlates.json").then(function (resp) {
     let allPlates = resp.data;
     let tag_element_div = document.getElementById("tag_element_div");
+    // 创建索引
+    createDivIndex();
+    for (const key in allPlates) {
+        createDivTabIndex(key);
+    }
+
     // 遍历对象中的所有键值对
     for (const key in allPlates) {
         createDiv(key);
-        createDivTab(key, allPlates[key]);
+        createDivTab(key + "_z", allPlates[key]);
     }
 });
 
@@ -45,11 +51,39 @@ var createDiv = function (key) {
     let divs = document.createElement("div");
     // 填充数据
     div.className = "tag_element";
+    div.id = key;
     h2.textContent = key;
     divs.className = "tag_element_div";
-    divs.id = key;
+    divs.id = key + "_z";
     // 链接数据
     div.appendChild(h2);
     div.appendChild(divs);
     document.getElementById("tag_element").appendChild(div);
+}
+
+// 创建索引外壳
+var createDivIndex = function () {
+    // 创建外壳
+    let div = document.createElement("div");
+    let h2 = document.createElement("h2");
+    let divs = document.createElement("div");
+    // 填充数据
+    div.className = "tag_element";
+    h2.textContent = "索引";
+    divs.className = "tag_element_div_index";
+    // 链接数据
+    div.appendChild(h2);
+    div.appendChild(divs);
+    document.getElementById("tag_element").appendChild(div);
+}
+
+// 创建索引标签内容
+var createDivTabIndex = function (keys) {
+    // 创建数据
+    let a = document.createElement("a");
+    // 填入内容
+    a.href = "#" + keys;
+    a.textContent = keys;
+    // 数据链接
+    document.getElementsByClassName("tag_element_div_index")[0].appendChild(a);
 }
