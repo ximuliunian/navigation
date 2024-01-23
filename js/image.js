46da1559edbc7758f0ad82184dc5e14be21abad9
曦暮流年 <2515550574@qq.com>
2024-01-23 10:13:32 +0800

    加载期更新

--- js/image.js
+++ js/image.js
@@ -1,3 +1,11 @@
+// 进行选取加载器图片
+axios.get("./json/loading.json").then(function (resp) {
+    let r = resp.data;
+    r = r[Math.floor(Math.random() * r.length)];
+    document.getElementById("loading").style.backgroundColor = r.bgcolor;
+    document.getElementById("loadingImg").src = r.img;
+})
+
 // 读取背景JSON文件
 axios.get("./json/image.json").then(function (resp) {
     let r = resp.data;
